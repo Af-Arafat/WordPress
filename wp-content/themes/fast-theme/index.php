34738563380bda37b3d472369bf53a66d6a9879c
Af-Arafat <ikramkhan6947@gmail.com>
2024-05-15 21:55:21 +0600

    new

--- wp-content/themes/fast-theme/index.php
+++ wp-content/themes/fast-theme/index.php
@@ -1,9 +1,43 @@
-<?php 
+<!DOCTYPE html>
+<html <?php language_attributes() ?> >
+<head>
+    <meta <?php bloginfo('charset') ?> >
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php bloginfo('name') ?></title>
+   <?php wp_head() ?>
+</head>
+<body <?php body_class() ?>>
+    <header>
+        <div class="wrapper">
+            <h2> <?php bloginfo('name'); ?> </h2>
+            <p> <?php bloginfo('description') ?> </p>
+        </div>
+        <?php ?>
+    </header>
 
-if (have_posts()):
-    while(have_posts()):
-        the_post(); ?>
-        <!-- html  -->
-        <?php endwhile;
+    <main>
+        <?php 
+        if (have_posts()):
+            while (have_posts()):
+                the_post(); ?>
+                
+                <!-- html -->
+                <h3 class="article-title">
+                    <?php the_title() ?>
+                </h3>
+                <?php the_content() ?>
+
+        <?php
+        endwhile;
         endif;
-?>
+         ?>
+    </main>
+
+<footer>
+    <p>&copy; All Rights Reservrd</p>
+</footer>
+
+    <p>Source: index.php </p>
+    <?php wp_footer() ?>
+</body>
+</html>
