d156d7cc581fa640871e69f357f25e4b667d882b
Af-Arafat <ikramkhan6947@gmail.com>
2024-06-05 12:09:45 +0600

    test

--- wp-content/themes/fast-theme/index.php
+++ wp-content/themes/fast-theme/index.php
@@ -4,12 +4,12 @@
     <header>
         <div class="wrapper">
             <h2> <?php bloginfo('name'); ?> </h2>
-            <p> <?php bloginfo('description') ?> </p>
+            <p class="tagline"> <?php bloginfo('description') ?> </p>
         </div>
         <?php ?>
     </header> 
 
-    <main>
+    <main class="wrapper" >
         <?php 
         if (have_posts()):
             while (have_posts()):
@@ -17,7 +17,7 @@
                 
                 <!-- html -->
                 <h3 class="article-title">
-                    <?php the_title() ?>
+                    <a href="<?php the_permalink() ?>"><?php the_title() ?></a>
                 </h3>
                 <?php the_content() ?>
 
@@ -26,6 +26,6 @@
         endif;
          ?>
     </main>
-<p>Source: index.php </p>
+    <p>Source: index.php </p>
 
 <?php get_footer() ?>
